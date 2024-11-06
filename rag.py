29e8a0bc77cf631cf0c5105c427d35e54c4d5790
yurikimbmgf <yuri.kim@gatesfoundation.org>
2024-11-06 11:18:07 -0800

    Initial commit

---
+++ rag.py
@@ -0,0 +1,84 @@
+## Set the working directory to the directory where the script is located
+import os
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+## API
+from dotenv import load_dotenv
+load_dotenv()
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+
+
+## Data Ingestion
+from langchain_community.document_loaders import PyPDFLoader
+loader = PyPDFLoader("data/2024-11-jff.pdf")
+emo_docs=loader.load_and_split()
+
+
+## Doc splitting
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
+documents=text_splitter.split_documents(emo_docs)
+# print(documents[:5])
+
+
+## Ingesting in vectorDB
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+db = Chroma.from_documents(documents,OpenAIEmbeddings())
+
+## Testing querying
+query = "give me a paragraph summary of the JFF announcement"
+
+
+
+retriever = db.as_retriever(
+            search_type="similarity", search_kwargs={"k": 1}
+        )
+relevant_docs = retriever.invoke(query)
+print(relevant_docs)
+
+## LangChaing-Rag Code
+from langchain_community.chat_models import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+
+llm = ChatOpenAI(model="gpt-4o-mini")
+
+from langchain import hub
+prompt = hub.pull("empler-ai/rag-prompt")
+
+writer = prompt | llm | StrOutputParser()
+
+content=[doc.page_content for doc in relevant_docs]
+context = "\n\n".join(content)
+
+answer=writer.invoke({"question":query,"context":context})
+
+print(answer)
+
+## RagCitation
+import uuid
+def generate_uuid():
+    unique_id = uuid.uuid4()
+    return str(unique_id)
+
+
+context = []
+for document in content:
+    context.append(
+        {
+            "source_id": generate_uuid(),
+            "document": document,
+            "meta": [],
+        }
+    )
+
+from rag_citation import CiteItem, Inference
+
+inference = Inference(spacy_model="sm", embedding_model="md")
+cite_item = CiteItem(answer=answer, context=context)
+
+output=inference(cite_item)
+print(output.citation)
+output.missing_word
+output.hallucination
---
+++ readme.txt
@@ -0,0 +1,16 @@
+# RAG 
+## November 2024
+
+Trying to build out a RAG for internal use.
+
+### Sources
+https://github.com/rahulanand1103/rag-citation
+https://github.com/rahulanand1103/rag-citation/blob/main/docs/examples/3.example-langchain.ipynb
+
+https://zilliz.com/blog/retrieval-augmented-generation-with-citations
+
+https://python.langchain.com/docs/how_to/qa_citations/
+
+https://learnbybuilding.ai/tutorials/rag-from-scratch
+
+https://hackernoon.com/comprehensive-tutorial-on-building-a-rag-application-using-langchain
